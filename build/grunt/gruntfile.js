57e8afe9800bbb169fe00e7e0871a89601b3e79b
Jay Traband <jayt@ideablade.com>
2014-03-20 12:35:06 -0700

    ...

--- build/grunt/gruntfile.js
+++ build/grunt/gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  // Note: nuget packaging of the Breeze.client nuget is done in breeze.server.net ( not here).
+  
   var path = require('path');
 
   var srcDir = '../../src/';
