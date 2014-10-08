2cd3da692d21131f4df05a22442eddc49dd5a2de
Ward Bell <wardbell@hotmail.com>
2014-10-08 12:38:03 -0700

    Revised original ajaxAdapter test to use sinon stub

--- test/internal/ajaxAdapterTests.js
+++ test/internal/ajaxAdapterTests.js
@@ -1,13 +1,21 @@
 // ajaxAdapterTests
 (function (testFns) {
     var breeze = testFns.breeze;
+    var originalAjaxAdapter;
 
     module("ajaxAdapter", {
         setup: function () {
             testFns.setup();
+            originalAjaxAdapter = breeze.config.getAdapterInstance('ajax');
         },
         teardown: function () {
-
+            try {
+                originalAjaxAdapter.ajax.restore(); // restore if spied or stubbed
+            } catch (e) {
+                /* assume it wasn't a spy or stub; ignore error*/
+            }
+            // restore pre-test default adapter instance in case we changed it in a test
+            breeze.config.initializeAdapterInstance('ajax', originalAjaxAdapter.name, true);
         }
     });
 
@@ -16,35 +24,28 @@
       * Because Breeze bug "2590 dataserviceadapter throws if ajax call throws",
       * the exception bubbles out synchronously and the test fails
       *********************************************************/
-    test("exception in ajax adapter should be caught in promise", 1, function () {
-        // var em = newEm();
+    asyncTest("exception in ajax adapter should be caught in promise", 2, function () {
         var em = new breeze.EntityManager('/bad/address/');
         var wasCaught = false;
+        var err = new Error('some error in the ajax adapter');
 
-        var adapter = breeze.config.getAdapterInstance('ajax');
-        var origAjax = adapter.ajax;
         // Make sure the ajaxAdapter throws when used
         // if you leave this line and bug #2590 is not fixed, the test will fail
         // Regardless, if you comment this out, then the "bad url" will be caught properly
         // and passed to the catch and the test passes
-        adapter.ajax = function () { throw new Error('some error in the ajax adapter'); };
-        stop();
+        sinon.stub(originalAjaxAdapter, "ajax").throws(err);
         try {
-            EntityQuery.from("Todos").using(em).execute()
-                .catch(function () {
-                    wasCaught = true;
-                })
+            breeze.EntityQuery.from("Todos").using(em).execute()
+                .catch(function () { wasCaught = true;})
                 .finally(fin);
         } catch (e) {
             ok(false, "the attempt to query with bad adapter threw sync exception");
             start();
-        } finally {
-            // restore ajax adapter regardless
-            adapter.ajax = origAjax;
         }
 
         function fin() {
-            ok(wasCaught, "the attempt to query a bad endpoint was passed to the promise's catch");
+            ok(originalAjaxAdapter.ajax.threw(err), "ajax adapter threw expected error");
+            ok(wasCaught, "the exceptions was passed to the promise's catch");
             start();
         }
     });
