839c200369ff75c61f2667288cc87685c86d3ba1
Lukino2000 <lukino2000@gmail.com>
2015-02-13 00:01:02 +0100

    AMD corrections

--- src/b00_breeze.modelLibrary.ko.js
+++ src/b00_breeze.modelLibrary.ko.js
@@ -4,7 +4,7 @@
   } else if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
     // CommonJS or Node: hard-coded dependency on "breeze"
     factory(require("breeze"));
-  } else if (typeof define === "function" && define["amd"] && !breeze) {
+  } else if (typeof define === "function" && define["amd"]) {
     // AMD anonymous module with hard-coded dependency on "breeze"
     define(["breeze"], factory);
   }
@@ -97,7 +97,7 @@
         es5Descriptors[prop.name] = propDescr;
       }
     })
-    if (!__isEmpty(es5Descriptors)) {
+    if (!core.isEmpty(es5Descriptors)) {
       var extra = stype._extra;
       extra.es5Descriptors = es5Descriptors;
       stype._koDummy = ko.observable(null);
