2b86a3f48d5c7eb5c44dd1acb1769b4ed91434af
Marcel Good <marcelg@ideablade.com>
2016-07-15 16:07:30 -0700

    Update to 1.5.9

--- build/breeze.intellisense.js
+++ build/breeze.intellisense.js
@@ -1,4 +1,4 @@
-// Generated on: Fri Jul 15 2016 14:12:18 GMT-0700 (Pacific Daylight Time)
+// Generated on: Fri Jul 15 2016 16:06:49 GMT-0700 (Pacific Daylight Time)
 intellisense.annotate(breeze.core, {
   'Enum': function() {
     /// <signature>
