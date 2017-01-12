c5216f6673c83349638da373e335f168a4893217
Steve Schmitt <steves@ideablade.com>
2017-01-12 15:32:10 -0800

    Update to breeze 1.6.2

--- build/breeze.base.debug.js
+++ build/breeze.base.debug.js
@@ -14676,7 +14676,7 @@ var EntityManager = (function () {
       // eg may be undefined or null
       if (!eg) return;
       var entities = eg.getChanges();
-      if (selected && selected.length && entities.length) {
+      if (selected && selected.length) {
         selected = selected.concat(entities);
       } else {
         selected = entities;
@@ -14694,7 +14694,7 @@ var EntityManager = (function () {
       // eg may be undefined or null
       if (!eg) return;
       var entities = eg.getEntities(entityStates);
-      if (selected && selected.length && entities.length) {
+      if (selected && selected.length) {
         selected = selected.concat(entities);
       } else {
         selected = entities;
